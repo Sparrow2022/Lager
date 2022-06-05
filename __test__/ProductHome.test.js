2d72b13e5fe89d1e53d1e4e868e43fbe80493de0
Sparrow2022 <kate.dabrowska@outlook.com>
2022-06-05 12:58:14 +0200

    first test passes

--- __test__/ProductHome.test.js
+++ __test__/ProductHome.test.js
@@ -1,6 +1,8 @@
 import { render } from '@testing-library/react-native';
 import ProductHome from '../components/ProductHome';
 
+jest.mock("../components/ProductList", () => "ProductList");
+
 test('header should exist containing text Lagerförteckning', async () => {
     const { getByText } = render(<ProductHome />);
     const header = await getByText('Lagerförteckning');
