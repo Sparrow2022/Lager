22774cdbc788567f2db96f04aaa2c527c8441365
Sparrow2022 <kate.dabrowska@outlook.com>
2022-06-05 13:04:15 +0200

    second test passes

--- __test__/ProductHome.test.js
+++ __test__/ProductHome.test.js
@@ -3,6 +3,9 @@ import ProductHome from '../components/ProductHome';
 
 jest.mock("../components/ProductList", () => "ProductList");
 
+// USE CASE:
+// I lagerförteckningen ska det finnas en rubrik
+
 test('header should exist containing text Lagerförteckning', async () => {
     const { getByText } = render(<ProductHome />);
     const header = await getByText('Lagerförteckning');
