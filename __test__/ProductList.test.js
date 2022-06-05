29739e05844da296f715466805f599a1440b66ce
Sparrow2022 <kate.dabrowska@outlook.com>
2022-06-05 13:15:07 +0200

    some comments

---
+++ __test__/ProductList.test.js
@@ -0,0 +1,26 @@
+import { render } from '@testing-library/react-native';
+import ProductList from '../components/ProductList';
+
+// USE CASE: 
+// I Lagerförteckningen ska det finnas en lista med produkter. 
+// Listan ska innehålla produktens namn och lagersaldo.
+
+const products = [
+    { name: "Shampoo", stock: 2 },
+    { name: "Balsam", stock: 3 },
+    { name: "Tvål", stock: 15 },
+];
+
+const setProducts = () => false;
+
+test('List should contain three items', async () => {
+    const { getByText } = render(<ProductList products={products} setProducts={setProducts} />);
+
+    const shampoo = await getByText('Shampoo', { exact: false });
+    const balsam = await getByText('Balsam', { exact: false });
+    const soap = await getByText('Tvål', { exact: false });
+
+    expect(shampoo).toBeDefined();
+    expect(balsam).toBeDefined();
+    expect(soap).toBeDefined();
+});
